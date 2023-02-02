d8eaf02edbca390010407e049f61f9db382fb758
tvidon <87492606+tvidon@users.noreply.github.com>
2023-02-02 15:04:07 +0000

    Implement Ford-Fulkerson

--- src/Graph.java
+++ src/Graph.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Class representing a directional graph
@@ -12,20 +14,30 @@ public class Graph{
     private ArrayList<ArrayList<Edge>> mat;
 
     /**
+     * List of nodes
+     */
+    private ArrayList<Node> nodes;
+
+    /**
      * Default constructor
      */
-    public Graph(){};
+    public Graph(){
+        Node.resetCount();
+    };
     /**
      * Contructor for when the number of nodes is already known
      * @param size number of nodes in the graph
      */
     public Graph(int size){
         mat = new ArrayList<>(size);
+        nodes = new ArrayList<>(size);
+        Node.resetCount();
         for (int i = 0; i < size; i++){
             mat.add(new ArrayList<>(size));
             for (int j = 0; j < size; j++){
                 mat.get(i).add(null);
             }
+            nodes.add(new Node());
         }
     };
 
@@ -41,8 +53,9 @@ public class Graph{
 
     /**
      * Add a node to the graph
+     * @param node node to add
      */
-    public void addNode(){
+    public void addNode(Node node){
         int size = mat.size();
         // resize the already existing columns
         for (int i = 0; i < size; i++){
@@ -53,6 +66,8 @@ public class Graph{
         for (int j = 0; j < size+1; j++){
             mat.get(size).add(null);
         }
+        // add the node
+        nodes.add(node);
     }
 
     @Override
@@ -66,5 +81,119 @@ public class Graph{
             string += "\n";
         }
         return string.trim();
+    }
+
+    /**
+     * Run the Ford-Fulkerson algorithm
+     * @param source source node index in the adjancency matrix
+     * @param sink sink node index in the adjacency matrix
+     */
+    public void ff(int sourceIndex, int sinkIndex){
+        Node source = nodes.get(sourceIndex);
+        Node sink = nodes.get(sinkIndex);
+        int size = mat.size();
+        boolean optimal = false;
+        while (!optimal){
+            // reset marks
+            for (Node node : nodes){
+                node.setFrom(null);
+            }
+            // mark
+            Queue<Node> q = new LinkedList<>();
+            q.add(source);
+            boolean foundSink = false;
+            do{
+                Edge edge;
+                Node currentNode = q.poll();
+                Node nextNode;
+                int residualCapacity;
+                int usedCapacity;
+                // go through successorcs and predecessors
+                for (int i=0; i<size; i++){
+                    // successors
+                    edge = mat.get(currentNode.getIndex()).get(i);
+                    // check if there is an edge
+                    if (edge != null){
+                        // check if the node is already marked
+                        nextNode = nodes.get(i);
+                        if (nextNode.getFrom() == null){
+                            // check if there is some residual capacity
+                            residualCapacity = edge.findResidual();
+                            if (residualCapacity > 0){
+                                q.add(nextNode);
+                                nextNode.setFrom(currentNode);
+                                nextNode.setSuccessor(true);
+                                if (nextNode == sink){
+                                    foundSink = true;
+                                }
+                            }
+                        }
+                    }
+                    // predecessors
+                    edge = mat.get(i).get(currentNode.getIndex());
+                    // check if there is an edge
+                    if (edge != null){
+                        // check if the node is already marked
+                        nextNode = nodes.get(i);
+                        if (nextNode.getFrom() == null){
+                            // check if there is some used capacity
+                            usedCapacity = edge.getUsed();
+                            if (usedCapacity > 0){
+                                q.add(nextNode);
+                                nextNode.setFrom(currentNode);
+                                nextNode.setSuccessor(false);
+                            }
+                        }
+                    }
+                }
+            } while (q.peek() != null & foundSink == false);
+            // increase if not optimal
+            if (foundSink){
+                Node previousNode;
+                Node currentNode;
+                // find the value to change by
+                currentNode = sink;
+                previousNode = currentNode.getFrom();
+                int min = mat.get(previousNode.getIndex()).get(currentNode.getIndex()).findResidual();
+                int res;
+                currentNode = previousNode;
+                previousNode = currentNode.getFrom();
+                while (currentNode != source){
+                    if (currentNode.getSuccessor()){
+                        res = mat.get(previousNode.getIndex()).get(currentNode.getIndex()).findResidual();
+                    }
+                    else{
+                        res = mat.get(currentNode.getIndex()).get(previousNode.getIndex()).getUsed();
+                    }
+                    if (res < min){
+                        min = res;
+                    }
+                    currentNode = previousNode;
+                    previousNode = currentNode.getFrom();
+                }
+                // improve flow
+                currentNode = sink;
+                previousNode = currentNode.getFrom();
+                Edge edge = mat.get(previousNode.getIndex()).get(currentNode.getIndex());
+                edge.setUsed(edge.getUsed() + min);
+                currentNode = previousNode;
+                previousNode = currentNode.getFrom();
+                while (currentNode != source){
+                    if (currentNode.getSuccessor()){
+                        edge = mat.get(previousNode.getIndex()).get(currentNode.getIndex());
+                        edge.setUsed(edge.getUsed() + min);
+                    }
+                    else{
+                        edge = mat.get(currentNode.getIndex()).get(previousNode.getIndex());
+                        edge.setUsed(edge.getUsed() - min);
+                    }
+                    currentNode = previousNode;
+                    previousNode = currentNode.getFrom();
+                }
+            }
+            else{
+                optimal = true;
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,35 @@ public class Main{
         Graph g = new Graph(2);
         System.out.println("default 2x2");
         System.out.println(g);
-        // 2x2 with edge from 1 to 0 with a capacity of 10
-        g.addEdge(1, 0, 10);
-        System.out.println("2x2 with edge from 1 to 0 with a capacity of 10");
+        // 2x2 with edge from 0 to 1 with a capacity of 10
+        g.addEdge(0, 1, 10);
+        System.out.println("2x2 with edge from 0 to 1 with a capacity of 10");
         System.out.println(g);
         // increase to 3x3
-        g.addNode();
+        g.addNode(new Node());
         System.out.println("increase to 3x3");
+        System.out.println(g);
+        // test ff
+        System.out.println("\ntest ff");
+        g = new Graph(9);
+        g.addEdge(0, 1, 30);
+        g.addEdge(0, 2, 30);
+        g.addEdge(0, 3, 20);
+        g.addEdge(1, 4, 20);
+        g.addEdge(1, 5, 20);
+        g.addEdge(2, 4, 20);
+        g.addEdge(2, 5, 20);
+        g.addEdge(3, 4, 20);
+        g.addEdge(3, 5, 20);
+        g.addEdge(4, 6, 10);
+        g.addEdge(5, 7, 60);
+        g.addEdge(6, 8, 30);
+        g.addEdge(7, 6, 20);
+        g.addEdge(7, 8, 50);
+        System.out.println("before");
+        System.out.println(g);
+        g.ff(0, 8);
+        System.out.println("after");
         System.out.println(g);
     }
 }
