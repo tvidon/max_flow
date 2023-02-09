be5fb982008c2d6ec8497735b7253a135e14d4d1
tvidon <87492606+tvidon@users.noreply.github.com>
2023-02-09 12:42:41 +0000

    Add x and y attributes to Node (will be useful for GUI, implementing now so as to avoid having to rewrite file reading and writing)

--- src/Node.java
+++ src/Node.java
@@ -17,6 +17,14 @@ public class Node{
      * Index in the adjacency matrix
      */
     private int index;
+    /**
+     * x in the gui
+     */
+    private int x;
+    /**
+     * y in the gui
+     */
+    private int y;
 
 
     /**
@@ -27,6 +35,34 @@ public class Node{
         this.index = index;
     };
 
+    /**
+     * Getter for x in the gui
+     * @return the x
+     */
+    public int getX(){
+        return x;
+    }
+    /**
+     * Getter for y in the gui
+     * @return the y
+     */
+    public int getY(){
+        return y;
+    }
+    /**
+     * Setter for x in the gui
+     * @param x x in the gui
+     */
+    public void setX(int x){
+        this.x = x;
+    }
+    /**
+     * Setter for y in the gui
+     * @param y y in the gui
+     */
+    public void setY(int y){
+        this.y = y;
+    }
     /**
      * Setter for source node for marking during the Ford-Fulkerson algorithm
      * @param node source node
